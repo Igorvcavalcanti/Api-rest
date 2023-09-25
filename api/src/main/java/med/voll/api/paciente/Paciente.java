c6dbe51e07cb37f647b30a4654345648549fec26
igorvcavalcanti <igorv3872@gmail.com>
2023-09-25 14:16:59 -0300

    Add cadastro de pacientes Create pacientes table Ajuste DadosDet

---
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,51 @@
+package med.voll.api.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+
+    @Embedded
+    private Endereco endereco;
+    private boolean ativo;
+
+    public Paciente (DadosCadastroPaciente dados){
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.cpf = dados.cpf();
+        this.email = dados.email();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.email() != null){
+            this.email = dados.email();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
+}
